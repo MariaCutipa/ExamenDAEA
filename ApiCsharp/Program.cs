031d8e6c290af05f7fc6c5d948a720a4db254079
MariaCutipa <129446025+MariaCutipa@users.noreply.github.com>
2024-09-01 20:41:55 -0500

    Update Program.cs

--- ApiCsharp/Program.cs
+++ ApiCsharp/Program.cs
@@ -65,7 +65,7 @@ public class RecomendacionService : IRecomendacionService
     public async Task<IEnumerable<Recomendacion>> GetRecomendacionesAsync(string searchTerm)
     {
         var response = await _httpClient.GetStringAsync(
-            $"http://ip172-18-0-39-cr905iqim2rg00fp8tt0-5000.direct.labs.play-with-docker.com/recomendacion/{searchTerm}");
+            $"http://ip172-18-0-4-crafn8aim2rg00dvbln0-5000.direct.labs.play-with-docker.com/recomendacion/{searchTerm}");
         var data = JArray.Parse(response);
 
         var recomendacions = new List<Recomendacion>();
