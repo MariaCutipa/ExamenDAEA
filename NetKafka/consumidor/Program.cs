5c14e6b2f6f0eb092a33e2aa016bbcda16ea2dbd
MariaCutipa <129446025+MariaCutipa@users.noreply.github.com>
2024-09-09 15:31:43 -0500

    Update Program.cs

--- NetKafka/consumidor/Program.cs
+++ NetKafka/consumidor/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string brokerList = "ec2-3-91-157-108.compute-1.amazonaws.com:9092"; // Dirección de tu broker de Kafka
+        string brokerList = "ec2-3-94-119-156.compute-1.amazonaws.com:9092"; // Dirección de tu broker de Kafka
         string topic = "products-topic"; // Nombre del topic en Kafka
 
         var config = new ConsumerConfig
